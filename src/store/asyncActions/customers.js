094c19ef8fc8ed4156f309dcca2d5d7dd8544826
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-06-08 13:28:56 +0400

    .then(json => dispatch(addManyCustomersAction(json))) // когда получены данные от сервера вызываю тот dispatch который прокинут через параметры и в него прокинула экшенкрейтор который вернёт экшен в моём случае добавить много пользователей и внего (json)это массив пользователей который пришёл от сервера

--- src/store/asyncActions/customers.js
+++ src/store/asyncActions/customers.js
@@ -1,6 +1,13 @@
 // тут делаю запросы к внешнему api
 
+import { addManyCustomersAction } from "../customerReducer"
+
 
 export const fetchCustomers = ()=> {
-    return function(dispatch)// должна вернуть новую фу-ию которая параметром принимает диспатч 
+    return function(dispatch){// должна вернуть новую фу-ию которая параметром принимает диспатч 
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(json => dispatch(addManyCustomersAction(json))) // когда получены данные от сервера вызываю тот dispatch который прокинут через параметры и в него прокинула экшенкрейтор который вернёт экшен в моём случае добавить много пользователей и внего (json)это массив пользователей который пришёл от сервера  
+
+    }
 }
